359ea31bcc4fb35e59978d1f44ab12699e637887
Michał Wojciechowski <michal.wojciechowski@outlook.com>
2019-02-10 23:27:39 +0100

    Dodaj pliki projektów.

---
+++ TestTalixo/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using  OpenQA.Selenium.Firefox;
+using static TestTalixo.SeleniumSet;
+
+namespace TestTalixo
+{
+    class Program
+
+     
+    {
+        IWebDriver driver = new ChromeDriver();
+        List<string> mails = new List<string>() { "invalid9@mail.com", "jan@o2.pl" };
+        static void Main(string[] args)
+        {
+           
+        }
+
+        [SetUp]
+        public void Initialize()
+        {
+            driver.Navigate().GoToUrl("https://talixo.pl/login/?next=/");
+            driver.Manage().Window.Maximize();
+            Console.WriteLine("Opened URL");
+        }
+
+        [Test]
+        public void ExecuteTest()
+        {
+            Console.WriteLine("Doing TEST:");
+
+            foreach (string mail in mails)
+            {
+                EnterText(driver, "id_email_auth", mail, "Id");
+                EnterText(driver, "password", "Test123", "Name");
+                Click(driver, "sign-in", "Name");
+                Error(driver);
+                ClearText(driver, "id_email_auth", "Name");
+            }
+
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            driver.Close();
+            Console.WriteLine("Closed Browser");
+        }
+    }
+}
---
+++ TestTalixo/SeleniumSet.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace TestTalixo
+{
+    public class SeleniumSet
+
+    {
+        static int i = 1;
+        public static void EnterText(IWebDriver driver, string element, string value, string elementType)
+        {
+            if (elementType == "Id")
+            {
+                driver.FindElement(By.Id(element)).SendKeys(value + Keys.Tab);
+            }
+
+            if (elementType == "Name")
+            {
+                driver.FindElement(By.Name(element)).SendKeys(value + Keys.Tab);
+                
+            }
+            
+        }
+
+        public static void Click(IWebDriver driver, string element, string elementType)
+        {
+            if (elementType == "Id")
+                driver.FindElement(By.Name(element)).Click();
+
+            if (elementType == "Name")
+                driver.FindElement(By.Name(element)).Click();
+
+        }
+
+        public static void ClearText(IWebDriver driver, string element, string elementType)
+        {
+            try
+            {
+                driver.FindElement(By.Id(element)).Clear();
+
+            }
+            catch (NoSuchElementException)
+            {
+                
+            }
+
+        }
+        
+
+        public static void Error(IWebDriver driver)
+        {
+            try
+            {
+                string error = driver.FindElement(By.ClassName("errorlist")).FindElement(By.TagName("li")).Text;
+                Console.WriteLine("Login error # "+i+" text: "+ error);
+                
+
+            }
+            catch (NoSuchElementException)
+            {
+                Console.WriteLine("Login successful");
+            }
+            i++;
+        }
+
+    }
+}
